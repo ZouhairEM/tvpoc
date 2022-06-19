b91d0b0b9980fa249cd9fc27bd1ec672a9afdee5
ZouhairEM <highaeonkratex@gmail.com>
2022-06-19 15:43:37 +0200

    Added search functionality

--- src/components/views/ShowsOverview.vue
+++ src/components/views/ShowsOverview.vue
@@ -1,8 +1,16 @@
  <template>
   This is the overview
   <div class="container">
+    <div class="row col-3 m-3 mx-auto">
+      <div class="col-9">
+        <input type="text" v-model="search" placeholder="Type to find a show" class="p-1" />
+      </div>
+      <div class="clear-btn col-3 bg-dark bg-gradient text-white p-1">
+        Clear
+      </div>
+    </div>
     <div class="row">
-      <div class="show-item col-md-4" v-for="(show, index) in shows" :key="index" @click="shareShow(show)">
+      <div class="show-item col-md-4" v-for="(show, index) in filteredShow" :key="index" @click="shareShow(show)">
         <ShowBio :showInfo="show" />
       </div>
     </div>
@@ -10,34 +18,50 @@
 </template>
  
  <script>
-import ShowBio from "../ShowBio.vue"
-export default {
-  name: "ShowsOverview",
-  components: {
-    ShowBio
-  },
-  data() {
-    return {
-      shows: [],
-    }
-  },
-  methods: {
-    fetchAPI: function () {
-      fetch("https://api.tvmaze.com/shows")
-        .then((response => {
-          return response.json();
-        }))
-        .then((response => {
-          this.shows = response;
-        }))
-    },
-    shareShow: function (showObj) {
-      this.$router.push({ name: "shows", params: { data: showObj } });
-      // console.log(JSON.parse(JSON.stringify(showObj)));
-    }
-  },
-  created() {
-    this.fetchAPI()
-  }
-}
-</script>
+ import ShowBio from "../ShowBio.vue"
+ export default {
+   name: "ShowsOverview",
+   components: {
+     ShowBio
+   },
+   data() {
+     return {
+       shows: [],
+       search: ""
+     }
+   },
+   methods: {
+     fetchAPI: function () {
+       fetch("https://api.tvmaze.com/shows")
+         .then((response => {
+           return response.json();
+         }))
+         .then((response => {
+           this.shows = response;
+         }))
+     },
+     shareShow: function (showObj) {
+       this.$router.push({ name: "shows", params: { data: showObj } });
+       // console.log(JSON.parse(JSON.stringify(showObj)));
+     }
+   },
+   computed: {
+     filteredShow: function () {
+        return this.shows.filter((show) => {
+          return (
+              show.name.toLowerCase().indexOf(this.search.toLowerCase()) !== -1
+          )
+        })
+     }
+   },
+   created() {
+     this.fetchAPI()
+   }
+ }
+ </script>
+ 
+ <style scoped>
+ .clear-btn {
+  line-height: 1.7;
+ }
+ </style>
